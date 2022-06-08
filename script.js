6223d07738dec664a50b967246e5329cca31410e
Ali Sharif <alisharif29@gmail.com>
2022-06-07 18:42:34 -0700

    Create computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello")
+
+function computerPlay (){
+    let randomNumber = Math.floor(Math.random()*3) + 1;
+    if(randomNumber === 1){
+        return 'Rock'
+    } else if(randomNumber === 2){
+        return 'Paper'
+    } else {
+        return 'Scissor'
+    }
+}
+
+
+console.log(computerPlay())
