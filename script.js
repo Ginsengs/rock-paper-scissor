63ad4014039509026a06aebc852fb4e13bb69644
Ali Sharif <alisharif29@gmail.com>
2022-06-07 19:00:27 -0700

    Create winning conditions

--- script.js
+++ script.js
@@ -10,5 +10,26 @@ function computerPlay (){
     }
 }
 
+function playRound(playerSelection, computerSelection){
+        playerSelection = playerSelection.toLowerCase();
+        computerSelection = computerSelection.toLowerCase();
 
-console.log(computerPlay())
+        if(playerSelection === 'rock ' && computerSelection === 'scissor'){
+            return 'You Win! Rock beats Scissor'
+        } else if(playerSelection === 'scissor' && computerSelection === 'rock'){
+            return 'You Lose! Rock beats Scissor'
+        } else if(playerSelection === 'scissor' && computerSelection ==='paper'){
+            return 'You Win! Scissor beats Paper'
+        } else if(playerSelection === 'paper' && computerSelection == 'scissor'){
+            return 'You Lose! Scissor beats Paper'
+        } else if(playerSelection === 'paper' && computerSelection ==='rock'){
+            return 'You Win! Paper beats Rock'
+        } else if(playerSelection=== 'rock' && computerSelection === 'paper') {
+            return 'You Lose! Paper beats Rock'
+        } else {
+            return 'Tie!'
+        }
+
+}
+
+console.log(playRound('Scissor', computerPlay()))
