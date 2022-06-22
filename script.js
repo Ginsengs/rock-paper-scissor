3b9a4493141f43e8c09af3ba2563fb1d6c4d8fda
Ali Sharif <alisharif29@gmail.com>
2022-06-21 20:46:06 -0700

    Finish no loop game function

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ function computerPlay (){
 function playRound(playerSelection, computerSelection){
         playerSelection = playerSelection.toLowerCase();
         computerSelection = computerSelection.toLowerCase();
+  
 
-        if(playerSelection === 'rock ' && computerSelection === 'scissor'){
+        if(playerSelection === 'rock' && computerSelection === 'scissor'){
             return 'You Win! Rock beats Scissor'
 
         } else if(playerSelection === 'scissor' && computerSelection === 'rock'){
@@ -35,27 +36,56 @@ function playRound(playerSelection, computerSelection){
 
 }
 
-function game(){
-    let computerWins = 0;
-    let userWins = 0;
-    for(let i =0; i < 5; i++){
-        playerSelection = prompt('Rock, Paper, Scissor: ')
-        let results = playRound(playerSelection, computerPlay());
-        if(results.includes('You Win')){
-            userWins = userWins + 1;
-        } else if(results.includes('You Lose')){
-            computerWins = computerWins + 1
-        } else {
+const buttons = document.querySelectorAll('.btn');
 
-        }
-    }
-    if(userWins > computerWins){
-        return 'You Win!'
-    } else if(userWins < computerWins){
-        return 'You Lose!'
-    } else {
-        return 'Tie!'
-    }
+buttons.forEach(but => but.addEventListener('click', myFunction));
+
+
+
+
+
+function myFunction(e){
+     let playerSelection = e.target.classList['0'];
+     let computerSelection = computerPlay();
+
+    content.textContent = playRound(playerSelection, computerSelection);
+    result.appendChild(content);
+    console.log(playerSelection);
+    console.log(computerSelection);
+
 }
 
-console.log(game());
+const result = document.querySelector('.result');
+const content = document.createElement('h1');
+
+
+
+
+
+
+//   playRound(playerSelection, computerPlay())
+
+// function game(){
+//     let computerWins = 0;
+//     let userWins = 0;
+//     for(let i =0; i < 5; i++){
+//         playerSelection = prompt('Rock, Paper, Scissor: ')
+//         let results = playRound(playerSelection, computerPlay());
+//         if(results.includes('You Win')){
+//             userWins = userWins + 1;
+//         } else if(results.includes('You Lose')){
+//             computerWins = computerWins + 1
+//         } else {
+
+//         }
+//     }
+//     if(userWins > computerWins){
+//         return 'You Win!'
+//     } else if(userWins < computerWins){
+//         return 'You Lose!'
+//     } else {
+//         return 'Tie!'
+//     }
+// }
+
+console.log('Files are linked properly');
