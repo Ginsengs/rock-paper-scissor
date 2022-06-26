3da2e5b4d77798355e269e208e6bbdaf437e5fc4
Ali Sharif <alisharif29@gmail.com>
2022-06-25 21:03:46 -0700

    finished assignment

--- script.js
+++ script.js
@@ -42,22 +42,55 @@ buttons.forEach(but => but.addEventListener('click', myFunction));
 
 
 
-
+let computerWins = 0;
+let userWins = 0;
 
 function myFunction(e){
-     let playerSelection = e.target.classList['0'];
-     let computerSelection = computerPlay();
 
-    content.textContent = playRound(playerSelection, computerSelection);
-    result.appendChild(content);
+    let playerSelection = e.target.classList['0'];
+    let computerSelection = computerPlay();
+    let results = playRound(playerSelection, computerPlay());
+
+    result.appendChild(userScore);
+    result.appendChild(computerScore);
     console.log(playerSelection);
     console.log(computerSelection);
 
+    if(results.includes('You Win')){
+        userWins = userWins + 1;
+    } else if(results.includes('You Lose')){
+        computerWins = computerWins + 1
+    } else {
+
+    }
+
+    userScore.textContent = 'Your Score: ' +  userWins;
+    computerScore.textContent = 'Computer Score: ' + computerWins
+
+    if(userWins >= 5 || computerWins >= 5){
+        let finalResult = scoreKeeper(userWins, computerWins);
+        gameResult.textContent = finalResult;
+        result.appendChild(gameResult);
+    }
+
+
+
+}
+
+function scoreKeeper(userWins, computerWins){
+    buttons.forEach(but => but.removeEventListener('click', myFunction));
+    if(userWins >= 5 && userWins > computerWins){
+        return 'You Win'
+   } else if(computerWins >= 5 && computerWins > userWins){
+        return 'You Lose'
+   }
+   
 }
 
 const result = document.querySelector('.result');
-const content = document.createElement('h1');
-
+const userScore = document.createElement('h2');
+const computerScore = document.createElement('h2')
+const gameResult = document.createElement('h2')
 
 
 
@@ -69,7 +102,7 @@ const content = document.createElement('h1');
 //     let computerWins = 0;
 //     let userWins = 0;
 //     for(let i =0; i < 5; i++){
-//         playerSelection = prompt('Rock, Paper, Scissor: ')
+
 //         let results = playRound(playerSelection, computerPlay());
 //         if(results.includes('You Win')){
 //             userWins = userWins + 1;
